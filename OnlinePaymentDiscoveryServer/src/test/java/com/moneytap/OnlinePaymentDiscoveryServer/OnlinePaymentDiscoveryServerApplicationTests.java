449a2f2cf7855eef990d8a80ba537a665565c8d9
ChiragChiranjib <58375906+ChiragChiranjib@users.noreply.github.com>
2021-08-17 13:48:05 +0530

    Add files via upload

---
+++ OnlinePaymentDiscoveryServer/src/test/java/com/moneytap/OnlinePaymentDiscoveryServer/OnlinePaymentDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneytap.OnlinePaymentDiscoveryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePaymentDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
